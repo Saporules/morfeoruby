863d0321144e70189b8e25aa41b8f0c37bb8aa2b
SapoRules <asaf_lopez@outlook.com>
2013-05-26 05:54:04 -0500

    Nah no funciono :(

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.scoped
+    @posts = Post.all
 
     respond_to do |format|
       format.html # index.html.erb
